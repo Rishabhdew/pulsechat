fb709724952fa4a1edd0ea7fa3a8c1b8f708beb4
Rishabh Dewangan <rishabhdewangan@Rishabhs-MacBook-Air.local>
2026-04-02 19:59:59 +0530

    Initial commit - PulseChat project

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:18
+
+# Create app directory
+WORKDIR /app
+
+# Copy package files
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy all files
+COPY . .
+
+# Expose port
+EXPOSE 3000
+
+# Start server
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve frontend
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
